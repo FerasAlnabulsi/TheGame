e4dcdb5b3a7ce75fcef363b053a61a0c7cd2e2a5
Haytham1995 <halabi_jb@hotmail.com>
2017-04-03 14:28:31 +0300

    added in game menu script
    no desc.

---
+++ Assets/Scripts/InGameMenu.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InGameMenu : MonoBehaviour {
+
+    public void PauseGame()
+    {
+        Time.timeScale = 0;
+    }
+
+    public void UnpauseGame()
+    {
+        Time.timeScale = 1;
+    }
+}
